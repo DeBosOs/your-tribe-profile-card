74323651fba8d5941b0eedb52ec7603fedb97dfa
DeBosOs <luc.hoogerwerf@hva.nl>
2024-09-02 12:35:31 +0200

    Les1
    Dit zijn de wijzigingen van les1

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 const expandButton = document.querySelector('header button')
 expandButton.addEventListener('click', expand)
 
 function expand () {
   document.body.classList.toggle('expand')
+=======
+const expandButton = document.querySelector('header button') //de knop wordt bekend gemaakt
+expandButton.addEventListener('click', expand) //wanneer er op de knop geklikt wordt, wordt de functie 'expand' uitgevoerd
+
+function expand () { //dat is dus deze functie
+  document.body.classList.toggle('expand') //de class 'expand' wordt toegevoegd aan de body van html
+>>>>>>> Stashed changes
 }
