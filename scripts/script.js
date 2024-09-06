4421110908fcf2377c57718a7172dfa06ddb0267
DeBosOs <luc.hoogerwerf@hva.nl>
2024-09-06 09:49:17 +0200

    nieuwe versie vrijdag 6 sept

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-
-const expandButton = document.querySelector('header button') //de knop wordt bekend gemaakt
-expandButton.addEventListener('click', expand) //wanneer er op de knop geklikt wordt, wordt de functie 'expand' uitgevoerd
-
-function expand () { //dat is dus deze functie
-  document.body.classList.toggle('expand') //de class 'expand' wordt toegevoegd aan de body van html
-
-}
